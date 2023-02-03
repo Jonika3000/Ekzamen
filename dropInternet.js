6732a7c1002ce3be617cc0b6f04ce419d3d15d73
Uamixerz <Uamixerz@gmail.com>
2023-02-03 13:36:15 +0200

    InternerComplete

--- dropInternet.js
+++ dropInternet.js
@@ -20,3 +20,14 @@ function closeDropDown() {
     document.getElementsByClassName("dropCatalog")[0].classList.add("offvisible");
     document.getElementsByClassName("dropCatalog")[0].classList.remove("visible");
 }
+
+function selectDrop($event) {
+    let item = document.getElementsByClassName("dropItemSelect")[0];
+    let btn = document.getElementsByClassName("btnDropContent")[0];
+    btn.textContent = $event.textContent;
+    item.classList.remove("dropItemSelect");
+    item.classList.add("dropItem");
+    $event.classList.remove("dropItem");
+    $event.classList.add("dropItemSelect");
+    closeDropDown();
+}
